d26c4e7b10001cf5744af0bb7d3482ac52436e10
Dina Oktavia Pangaribuan <140843745+sg-riley@users.noreply.github.com>
2025-04-03 02:47:46 +0700

    first commit

---
+++ src/components/main-navbar.tsx
@@ -0,0 +1,87 @@
+"use client";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "@/components/ui/resizable-navbar";
+import { useState } from "react";
+
+export function MainNavbar() {
+  const navItems = [
+    {
+      name: "Features",
+      link: "#features",
+    },
+    {
+      name: "Pricing",
+      link: "#pricing",
+    },
+    {
+      name: "Contact",
+      link: "#contact",
+    },
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <Navbar className="h-16 w-5/6 my-4 mx-auto rounded-lg flex-col items-center bg-blue-300/20 backdrop-blur-md shadow-md z-50 fixed top-0">
+      {/* Desktop Navigation */}
+
+      <NavBody className="h-full">
+        <NavbarLogo />
+        <NavItems items={navItems} />
+      </NavBody>
+
+      {/* Mobile Navigation */}
+      <MobileNav className="h-full">
+        <MobileNavHeader className="h-full px-4">
+          <NavbarLogo />
+          <MobileNavToggle
+            isOpen={isMobileMenuOpen}
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          />
+        </MobileNavHeader>
+
+        <MobileNavMenu
+          isOpen={isMobileMenuOpen}
+          onClose={() => setIsMobileMenuOpen(false)}
+          className="text-white"
+        >
+          {navItems.map((item, idx) => (
+            <a
+              key={`mobile-link-${idx}`}
+              href={item.link}
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="relative text-white font-[family-name:var(--font-plus-jakarta-sans)]"
+            >
+              <span className="block">{item.name}</span>
+            </a>
+          ))}
+          <div className="flex w-full flex-col gap-4">
+            <NavbarButton
+              onClick={() => setIsMobileMenuOpen(false)}
+              variant="primary"
+              className="w-full"
+            >
+              Login
+            </NavbarButton>
+            <NavbarButton
+              onClick={() => setIsMobileMenuOpen(false)}
+              variant="primary"
+              className="w-full"
+            >
+              Book a call
+            </NavbarButton>
+          </div>
+        </MobileNavMenu>
+      </MobileNav>
+    </Navbar>
+  );
+}
